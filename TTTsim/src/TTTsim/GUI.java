4c0a8a1c646df21a161292f059586b5332098a07
Gecko <geckontrv7@gmail.com>
2018-10-03 09:17:22 -0500

    Added some backbone to the gui

--- TTTsim/src/TTTsim/GUI.java
+++ TTTsim/src/TTTsim/GUI.java
@@ -1,5 +1,23 @@
 package TTTsim;
-
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 public class GUI {
-//yeeeet
+	JFrame frame;
+	JPanel panel;
+	JLabel label1, label2, label3, total;
+	JButton button;
+	JTextField text1, text2, text3;
+	public void GUI(){
+		frame = new JFrame("TicTacToe Simultor");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(500, 500);
+		frame.setLocationRelativeTo(null);
+		
+		
+		frame.setContentPane(panel);
+		frame.setVisible(true);
+}
 }
