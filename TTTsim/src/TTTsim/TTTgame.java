06bf5e8acfcf033c2ccf2b875074749026d0e0b9
Egg3234 <nospam@gmail.com>
2018-10-04 09:58:37 -0500

    Thurs Prog UD - TTTgame

--- TTTsim/src/TTTsim/TTTgame.java
+++ TTTsim/src/TTTsim/TTTgame.java
@@ -12,13 +12,33 @@ package TTTsim;
 import java.util.Random;
 
 public class TTTgame {
-	private int score1, score2, games, turn;
+	private int score1, score2, games, turn, win;
 	private int[][] board;
-	private int[][] win;
 
 	public TTTgame(int g) {
 		games = g;
+		turn = 1;
+		board = new int[3][3];
+		win = 0;
+		score1 = 0;
+		score2 = 0;
 		
+		for(int i=0; i<3; i++) {
+			for(int j=0; j<3; j++) {
+				board[i][j] = 0;
+			}
+		}
+
+	}
+	
+	/**
+	 * Plays a turn and returns a square
+	 * @return
+	 */
+	public int pTurn() {
+		
+		
+		return 0;
 
 	}
 
