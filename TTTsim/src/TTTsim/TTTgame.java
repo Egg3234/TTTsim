cdbddcff8b5d454ff91fe51330dfc9430ffee974
Egg3234 <nospam@gmail.com>
2018-10-05 09:57:40 -0500

    Friday updates

--- TTTsim/src/TTTsim/TTTgame.java
+++ TTTsim/src/TTTsim/TTTgame.java
@@ -13,20 +13,18 @@ import java.util.Random;
 
 public class TTTgame {
 	private int score1, score2, games, turn, win;
-	private int[][] board;
+	private int[] board;
 
 	public TTTgame(int g) {
 		games = g;
 		turn = 1;
-		board = new int[3][3];
+		board = new int[9];
 		win = 0;
 		score1 = 0;
 		score2 = 0;
 		
-		for(int i=0; i<3; i++) {
-			for(int j=0; j<3; j++) {
-				board[i][j] = 0;
-			}
+		for(int i=0; i<9; i++) {
+			board[i] = 0;
 		}
 
 	}
@@ -36,7 +34,23 @@ public class TTTgame {
 	 * @return
 	 */
 	public int pTurn() {
-		 
+		Random r = new Random();
+		int valid = 0;
+		int s;
+		
+		while(valid==0) {
+			s = r.nextInt(8-1+1)+1;
+			if(board[s]==0) {
+				if(turn%2==1) {
+					board[s] = 1;
+				}else if(turn%2==0) {
+					board[s] = 2;
+				}else {
+					System.out.println("Error 1: TTTg - 49");
+				}
+				valid = 1;
+			}
+		}
 		
 		return 0;
 
